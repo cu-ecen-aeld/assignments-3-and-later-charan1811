125ed4a377eea7975ec831c9a1c4c262053bc575
Sai Mandadi <sama7963@aesd-students.colorado.edu>
2024-01-20 06:18:03 +0000

    AESD updated comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-# A simple finder script
+# Author: Sai Charan Mandadi
+# Description: Finds the required string in the given input directories and prints the count of total
+#number of files and matched lines.
 
+#Check if the arguments are valid number of arguments
 if [ $# -ne 2 ];
 then
 	echo "ERROR: Invalid number of arguments"
 	echo "Total number of arguments: 2. usage: finder.sh <files dir> <search string>"
 	exit 1
 else
+#Checks if the input file directory exists and then extracts the total files and matched lines with the input string.
 	if [ -d "$1" ];
 	then
 		TOTAL_FILES=$(find $1 -type f | wc -l)
 		MATCHED_LINES=$(grep -r $2 $1 | wc -l)
 		echo "The number of files are ${TOTAL_FILES} and the number of matching lines are ${MATCHED_LINES}"
 		exit 0
+#Handles the condition where the input file directory is not present.
 	else
 		echo "Input File directory does not exits"
 		exit 1
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# A simple writer script
+#Author: Sai Charan Mandadi
+#Description: A simple writer script that takes file path and string to be inserted into the file.
+#creates the the file directory if the input path is not present.
 
+#Checks for valid input arguments
 if [ "$#" -gt 0 ] && [ "$#" -lt 3 ];
 then
+#If the file already exists insert the string into the file
 	if [ -f "$1" ];
 	then
 		echo $2 > $1
+#Create a file or directory if either of them is not present
 	else
 		if [ -d "$(dirname "$1")" ];
 		then
@@ -14,10 +19,18 @@ then
 		else
 			echo "Input file is not present, creating new file at $1"
 			mkdir -p "$(dirname "$1")"
-			touch $1
-			echo $2 > $1
+#checks if mkdir is able to execute without errors
+			if [ $? -ne 0 ];
+			then
+				echo "Unable to create input directory path"
+			else
+#Insert the input string into the newly created file
+				touch $1
+				echo $2 > $1
+			fi
 		fi
 	fi
+#Handles Invalid number of arguments
 else
 	echo "Please provide valid arguments"
 	exit 1;
