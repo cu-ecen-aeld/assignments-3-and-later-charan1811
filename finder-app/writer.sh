7b421501843b17db662235b26d6d8449626b5e34
Sai Mandadi <sama7963@aesd-students.colorado.edu>
2024-01-22 23:54:36 +0000

    Improved readability

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -6,27 +6,29 @@
 #Checks for valid input arguments
 if [ "$#" -gt 0 ] && [ "$#" -lt 3 ];
 then
+	INPUTFILE="$1"
+	INPUTSTRING="$2"
 #If the file already exists insert the string into the file
-	if [ -f "$1" ];
+	if [ -f "$INPUTFILE" ];
 	then
-		echo $2 > $1
+		echo $INPUTSTRING > $INPUTFILE
 #Create a file or directory if either of them is not present
 	else
-		if [ -d "$(dirname "$1")" ];
+		if [ -d "$(dirname "$INPUTFILE")" ];
 		then
-			touch $1
-			echo $2 > $1
+			touch $INPUTFILE
+			echo $INPUTSTRING > $INPUTFILE
 		else
 			echo "Input file is not present, creating new file at $1"
-			mkdir -p "$(dirname "$1")"
+			mkdir -p "$(dirname "$INPUTFILE")"
 #checks if mkdir is able to execute without errors
 			if [ $? -ne 0 ];
 			then
 				echo "Unable to create input directory path"
 			else
 #Insert the input string into the newly created file
-				touch $1
-				echo $2 > $1
+				touch $INPUTFILE
+				echo $INPUTSTRING > $INPUTFILE
 			fi
 		fi
 	fi
