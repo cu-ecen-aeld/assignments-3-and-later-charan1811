610dc53652b484becaeabfbd87813cf9056aa5ab
Charan <charan@Ubuntu.myguest.virtualbox.org>
2024-02-07 13:23:02 -0700

    assign-4 p1 commit

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,6 +14,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
+     pthread_mutex_t* m_mutex;
+     int wait_obtain_ms;
+     int wait_release_ms;
 
     /**
      * Set to true if the thread completed with success, false
